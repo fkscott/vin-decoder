67e5c673c6498b6480738f001404730108c6140e
Keagan <fkscott@loyola.edu>
2022-01-09 21:39:56 -0500

    remove images add dark color scheme

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
 	//boolean for loading spinner
 	let hidden = true;
 
-	//
 
 	let vehicleInfo = [];
 	let vinSearch = "";
@@ -43,7 +42,7 @@
 
 <main>
 
-	<div class="container">
+	<div class="container text-light">
 
 		<div class="my-5 row text-center">
 			<div class="col-md-12">
@@ -55,12 +54,12 @@
 		<div class="my-5 row">
 			<div class="col-md-12 text-center">
 				
-				<h2>Vin Lookup</h2>
-				<div class="form-floating mb-3">
+				<h2>VIN Lookup</h2>
+				<div class="form-floating mb-3 text-dark">
 					<input type="text" class="form-control" placeholder="Enter a VIN#" id="floatingVinInput" bind:value={vinSearch} />
 					<label for="floatingnVinInput">Enter a VIN#</label>
 				</div>
-				<div class="form-floating mb-3">
+				<div class="form-floating mb-3 text-dark">
 
 					<input type="text" class="form-control" placeholder="Enter Your Model Year" id="floatingYearInput" bind:value={modelYear} />
 					<label for="floatingYearInput">Enter Your Model Year</label>
@@ -71,10 +70,7 @@
 				<div class="vin-table text-center">
 
 					{#if vehicleInfo.length > 0 }
-					<div class="my-3">
-						<img src="./assets/BodyClassImages/{vehicleInfo[21].ValueId}.png" id="bodyClassIcon" class="img-fluid" alt="Body Class Icon">
-					</div>
-					<table class="table table-bordered table-striped bdr">
+					<table class="table table-dark table-bordered table-striped bdr">
 
 					<tbody class="mt-3">
 						<tr>
